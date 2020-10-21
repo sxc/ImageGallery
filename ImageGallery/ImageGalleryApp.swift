7fc3bb07dee684b019bca9b74b2c9bd40bbfc7ad
sxc <qiuyequ@icloud.com>
2020-10-21 09:58:55 +0800

    Initial Commit

---
+++ ImageGallery/ImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryApp.swift
+//  ImageGallery
+//
+//  Created by Xiaochun Shen on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
